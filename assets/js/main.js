b7b18bda653152aae121f63953269357e10bc814
Luciana Sena <coder.lucianasena@gmail.com>
2024-11-17 12:14:57 -0300

    Correções Services Section

--- assets/js/main.js
+++ assets/js/main.js
@@ -98,6 +98,29 @@ function portfolioItemDetails(portfolioItem) {
     document.querySelector(".portfolio__popup-body").innerHTML = portfolioItem.querySelector(".portfolio__item-details").innerHTML;
 }
 
+// Serviços Modal
+const modalViews = document.querySelectorAll('.services__modal'),
+      modalBtns = document.querySelectorAll('.services__button'),
+      modalCloses = document.querySelectorAll('.services__modal-close');
+
+let modal = function(modalClick) {
+    modalViews[modalClick].classList.add('active-modal');
+}
+
+modalBtns.forEach((modalBtn, i) => {
+    modalBtn.addEventListener('click', () => {
+        modal(i);
+    });
+});
+
+modalCloses.forEach((modalClose) => {
+    modalClose.addEventListener('click', () => {
+        modalViews.forEach((modalView) => {
+            modalView.classList.remove('active-modal');
+        });
+    });
+});
+
 // SWIPER TESTIMONIAL
 // let swiper = new Swiper(".testimonials__container", {
 //     spaceBetween: 24,
